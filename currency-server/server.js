98cb942e4a66fafd61b1a7029f37b9741b6646c4
Tamer Mahmoud <tamer.m.mahmoud@gmail.com>
2025-07-21 13:48:43 -0400

    Update server.js

--- currency-server/server.js
+++ currency-server/server.js
@@ -3,11 +3,19 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
 app.use(cors());
 app.use(express.json());
 
 const conversionRoutes = require('./routes/conversion');
 app.use('/api', conversionRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not found' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
